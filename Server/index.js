b93f838fe5aa3db5a76af227009e29bb1caa786c
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-01 14:22:59 -0300

    Creo comunicación client-server

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,25 @@
 const { PORT } = require('./config.js'); 
 const express = require('express');
+const http = require('http');
+const socket_io = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hola Server')
+const server = http.createServer(app);
+const io = socket_io(server, {cors: {origin: "http://localhost:3000"}});
+
+io.on('connection', (socket) => {
+    
+    socket.on("desde cliente", (arg) => { // RECIBO DESDE EL CLIENTE
+        console.log(socket.id);
+        console.log(arg);
+        //socket.emit("desde server",  `el servidor confirma que recibio tu mensaje: ${arg} desde el cliente ID: ${socket.id}`);
+        io.emit("desde server", `${arg}/${socket.id}`) // ENVIO DESDE EL SERVER
+    })
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,31 @@
-import './App.css';
+// import './App.css';
+import { useState, useEffect } from 'react';
+const { io } = require("socket.io-client");
+
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const socket = io("http://localhost:4000/");
+
+  socket.on("desde server", (arg) => { // RECIBO DESDE EL SERVER
+    const socketId = arg.split("/")[1];
+    const socketMsj = socketId === socket.id ? "_____________" + arg.split("/")[0] : arg.split("/")[0];
+
+    socketMsj && setMessages([socketMsj, ...messages]) // DISPARA EL RENDERIZADO
+  });
+
+  useEffect(() => { // SE EJECUTA LUEGO DE MODIFICAR LA VARIABLE MESSAGE
+    socket.emit("desde cliente", message); // ENVIO DESDE EL CLIENTE
+  }, [message]);
+
+  const handler = (e) => {
+   e.preventDefault();
+   const textValue = e.target.firstChild.value;
+   textValue && setMessage(textValue) // DISPARA EL RENDERIZADO y EL USE EFFECT
+   e.target.firstChild.value = "";
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -8,6 +33,15 @@ function App() {
           Hola Cliente
         </p>
       </header>
+      <body>
+        <form onSubmit={handler}>
+          <input type='text' defaultValue={message} placeholder="Write a message"></input>
+          <button>SEND</button>
+        </form>
+        {
+          messages.map((message, index) => <p key={index}>{message}</p>)
+        }
+      </body>
     </div>
   );
 }
